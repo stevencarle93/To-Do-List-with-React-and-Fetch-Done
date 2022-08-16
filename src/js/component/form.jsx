8e2bf71f9810a4cf2fbf3e9af9f1d5cd0f459b1d
stevencarle93 <stevencarle@hotmail.com>
2022-08-16 23:50:34 +0000

    Changes Done August 16 2022

--- src/js/component/form.jsx
+++ src/js/component/form.jsx
@@ -1,15 +1,14 @@
-import React, {  useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import TaskList from "./task_list.jsx";
 
 export default function Form() {
+  const [tasks, setTask] = useState([]);
+  const [input, setInput] = useState("");
+  const apiUrl = "https://assets.breatheco.de/apis/fake/todos/user/stevenc";
 
-	const [tasks, setTask] = useState([])
-	const [input, setInput] = useState("")
-	const apiUrl="https://assets.breatheco.de/apis/fake/todos/user/stevenc"
-	
-	useEffect(()=>{
-		// componentDidMount
-		fetch(apiUrl)
+  useEffect(async () => {
+    // componentDidMount
+    /* fetch(apiUrl)
 		.then(res=>{
 			if(res.ok) return res.json()
 			else{
@@ -18,90 +17,121 @@ export default function Form() {
 			}
 		})
 		.then(data=>setTask(data))
-		.catch(error=>console.error(error))
-	},[])
+		.catch(error=>console.error(error)) */
 
-	useEffect(()=>{
-		// componentDidUpdate
-		fetch(apiUrl,{
-			method:"PUT",
-			body:JSON.stringify(tasks),
-			headers:{"Content-Type":"application/json"}
-		})
-		.then(res=>{
-			console.log(res.status + ": " + res.statusText)
-		})
-		.catch(error=>console.error(error))
-	},[tasks])
+    /*  let response = await fetch(apiUrl) //obj promesa
+		if(response.ok){
+			response = await response.json() //obj javascript
+		}else{
+			crearLista()
+		} */
 
-	const crearLista= ()=>{
-		fetch(apiUrl,{
-			method:"POST",
-			body:JSON.stringify([]),
-			headers:{
-				"Content-Type":"application/json"
-			}
-		}).then(res=>console.log(res.status))
-		.catch(error=>console.error(error))
-	}
+    try {
+      let response = await fetch(apiUrl); //obj promesa
+      response = await response.json(); //obj javascript
+    } catch {
+      crearLista();
+      console.log("effect");
+    }
+  }, []);
 
-	const handleChange = (event) => {
-		setInput(event.target.value)
-	}
+  useEffect(() => {
+    // componentDidUpdate
+    console.log(tasks);
+    fetch(apiUrl, {
+      method: "PUT",
+      body: JSON.stringify(tasks),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => {
+        console.log(res.status + ": " + res.statusText);
+      })
+      .catch((error) => console.error(error));
+  }, [tasks]);
 
-	const infoSubmited = (event) => {
-		event.preventDefault()
-		const data = new FormData(event.target)
-		setTask([...tasks, data.get("task")])	
-		event.target.reset()
-	}
+  const crearLista = () => {
+    fetch(apiUrl, {
+      method: "POST",
+      body: JSON.stringify([]),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => console.log(res.status))
+      .catch((error) => console.error(error));
+  };
 
-	const removeTask = indexItem => {
-		setTask(prevState =>
-			prevState.filter((todo, index) => index !== indexItem)
-		);
-	};
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
 
-	let allTasks = tasks.map((elements, index) => {
-		if(tasks != "")
-			return(<li key={index} className="list-group-item d-flex justify-content-between"> 
-				{elements.label}: {elements.done?"Listo":"Pendiente"}
-				<button onClick={() => removeTask(index)}>
-					Remove
-				</button>
-			</li>)
-		else 
-			return "No task"
-		
-	})
-				
+  const infoSubmited = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    setTask([
+      ...tasks,
+      {
+        label: `Task: ${data.get("task2")} ; Status `,
+        done: false,
+      },
+    ]);
+    event.target.reset();
+  };
 
-	return (
+  const removeTask = (indexItem) => {
+    setTask((prevState) =>
+      prevState.filter((todo, index) => index !== indexItem)
+    );
+  };
 
-        <div className="text-center" style={{ marginTop: "1rem" }}>
-			
-			<form id="Form" onSubmit={infoSubmited}>
-				<div className="form-group d-flex justify-content-center" id = 'font-link'>	
-                        <input className="form-control" type="text" 
-							name="task" style={{width: "50rem"}}
-							id="task" placeholder="what do you want to do?"
-							value={input} onChange={(handleChange)}/>
-				</div>
-				<div className="form-group" style={{ marginTop: "1rem" }}>					
-                        <button type="submit" className="btn btn-light" 
-						    style={{background: "white"}}>
-							Submit
-						</button>
-				</div>
-			</form>
+  let allTasks = tasks.map((elements, index) => {
+    if (tasks != "")
+      return (
+        <li
+          key={index}
+          className="list-group-item d-flex justify-content-between"
+        >
+          {elements.label}: {elements.done ? "Done" : "Pending"}
+          <button onClick={() => removeTask(index)}>Remove</button>
+        </li>
+      );
+    else return "No task";
+  });
 
-			{
-				//<TaskList tasks={allTasks}/>
-				allTasks
-			}
-			<h3>
-				Tasks pending: {tasks.length}
-			</h3>
-		</div>
-	);
-};
+  return (
+    <div className="text-center" style={{ marginTop: "1rem" }}>
+      <form id="Form" onSubmit={(e) => infoSubmited(e)}>
+        <div
+          className="form-group d-flex justify-content-center"
+          id="font-link"
+        >
+          <input
+            className="form-control"
+            type="text"
+            name="task2"
+            style={{ width: "50rem" }}
+            id="task"
+            placeholder="what do you want to do?"
+            value={input}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group" style={{ marginTop: "1rem" }}>
+          <button
+            type="submit"
+            className="btn btn-light"
+            style={{ background: "white" }}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+
+      {
+        //<TaskList tasks={allTasks}/>
+        allTasks
+      }
+      <h3>Tasks pending: {tasks.length}</h3>
+    </div>
+  );
+}
